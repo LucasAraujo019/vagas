d21f4596acf66d383699128733881b9869cbba67
unknown <lucas.oliveira019@hotmail.com>
2024-11-01 22:07:09 -0300

    Removendo comentarios

--- globals.php
+++ globals.php
@@ -3,6 +3,5 @@
         session_start(); // Inicia a sessão apenas se ainda não estiver ativa
     }
     
-    // Define a URL base, garantindo o uso correto do protocolo e caminho
     $BASE_URL = "http://" . $_SERVER["HTTP_HOST"] . rtrim(dirname($_SERVER["SCRIPT_NAME"]), '/') . "/";
 ?>
