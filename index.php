6290c9bbfcb90e967eb0a6b62d419f54c895c695
unknown <lucas.oliveira019@hotmail.com>
2024-11-01 22:08:18 -0300

    Adicionando modal

--- index.php
+++ index.php
@@ -49,6 +49,25 @@ require_once("templates/header.php");
     </table>
 </div>
 
+<div class="modal fade" id="viewVagaModal" tabindex="-1" role="dialog" aria-labelledby="viewVagaModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header modal-header-custom">
+                <h5 class="modal-title" id="viewVagaModalLabel">Detalhes da Vaga</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body modal-body-custom" id="vagaDetails">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 <?php
 require_once("templates/footer.php");
 ?>
